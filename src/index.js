6ec74ce56abbc648ff8bc5310aa52361f15c3d8e
Andrés Muñoz <andres.munoz@idukay.com>
2022-01-02 11:05:09 -0500

    add chakra

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { HashRouter } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 
 ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
