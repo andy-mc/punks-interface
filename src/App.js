980406879adafb9693de8fcb8dbabfd9276353ad
Andrés Muñoz <andres.munoz@idukay.com>
2022-01-02 20:40:08 -0500

    add web3 orovider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import Home from './views/home';
+import Web3 from 'web3';
 
 function App() {
+  useEffect(async () => {
+    //   window.ethereum.request({ method: 'eth_requestAccounts' }).then((accounts) => {
+    //     console.log('accounts:', accounts);
+    //   });
+    // const web3 = new Web3(window.ethereum);
+    // const accounts = await web3.eth.requestAccounts();
+
+    // injecting the provider now all the app has provider = window.ethereum
+    const web3 = new Web3(window.ethereum);
+    web3.eth.requestAccounts();
+  }, []);
+
   return (
     <>
       <Route path="/" exact component={Home} />
