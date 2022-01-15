8536eb1945198c19ae41473d32e61ba5f11b02f6
Andrés Muñoz <andres.munoz@idukay.com>
2022-01-15 14:29:10 -0500

    create usePlatziPunksDataHook

--- src/views/punks/index.js
+++ src/views/punks/index.js
@@ -1,5 +1,20 @@
+import { useWeb3React } from '@web3-react/core';
+import Loading from '../../components/loading';
+import PunkCard from '../../components/punk-card';
+import RequestAccess from '../../components/request-access';
+
 const Punks = () => {
-  return<h1>Punks</h1>
+  const {active} = useWeb3React()
+  
+  if(!active) return <RequestAccess></RequestAccess>
+  
+  return(
+    <>
+    <h1>Gallery</h1>
+    <Loading></Loading>
+    <PunkCard></PunkCard>
+    </>
+  )
 }
 
 export default Punks;
