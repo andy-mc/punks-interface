f375033a434b587d62b579aa7fe309ed13b3e261
Andrés Muñoz <andres.munoz@idukay.com>
2022-01-15 15:18:46 -0500

    create punk card

--- src/views/punks/index.js
+++ src/views/punks/index.js
@@ -1,18 +1,27 @@
 import { useWeb3React } from '@web3-react/core';
+import { Grid, Text } from '@chakra-ui/react';
 import Loading from '../../components/loading';
 import PunkCard from '../../components/punk-card';
 import RequestAccess from '../../components/request-access';
+import {usePlatziPunksData} from '../../hooks/usePlatziPunksData';
 
 const Punks = () => {
-  const {active} = useWeb3React()
+  const {active} = useWeb3React();
+  const {loading, punks} = usePlatziPunksData();
   
-  if(!active) return <RequestAccess></RequestAccess>
-  
+
+  if (!active) return <RequestAccess></RequestAccess>
+
   return(
     <>
-    <h1>Gallery</h1>
-    <Loading></Loading>
-    <PunkCard></PunkCard>
+      {loading ? <Loading /> :
+      <>
+        <Text fontSize='5xl' color='gray.500' align="center" mb={4}>Gallery</Text>
+        <Grid templateColumns="repeat(auto-fill, minmax(250px, 1fr))" gap={6}>
+          {punks.map(({tokenId, name, image}) => <PunkCard key={tokenId} name={name} image={image} />)}
+        </Grid>
+      </>
+      }
     </>
   )
 }
