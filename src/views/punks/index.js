3a5c895d61b2b1f1091e40a88373a3a7aac0776d
Andrés Muñoz <andres.munoz@idukay.com>
2022-01-16 23:11:22 -0500

    add account filter to platzi punks list

--- src/views/punks/index.js
+++ src/views/punks/index.js
@@ -10,35 +10,64 @@ import {
   FormHelperText,
   FormControl,
 } from "@chakra-ui/react";
-import { SearchIcon } from "@chakra-ui/icons";
-import Loading from '../../components/loading';
-import PunkCard from '../../components/punk-card';
-import RequestAccess from '../../components/request-access';
-import {usePlatziPunksData} from '../../hooks/usePlatziPunksData';
 import { Link } from "react-router-dom";
+import PunkCard from '../../components/punk-card';
+import Loading from '../../components/loading';
+import RequestAccess from '../../components/request-access';
+import { SearchIcon } from "@chakra-ui/icons";
+import {usePlatziPunksData} from '../../hooks/usePlatziPunksData';
 import { useState } from 'react';
+import { useHistory, useLocation  } from 'react-router-dom';
 
 const Punks = () => {
-  const [address, setAddress] = useState('')
-  const {active} = useWeb3React();
-  const {loading, punks} = usePlatziPunksData();
+  const { search } = useLocation();
+  const [address, setAddress] = useState(
+    new URLSearchParams(search).get("address")
+  );
+  const [submitted, setSubmitted] = useState(true);
+  const [validAddress, setValidAddress] = useState(true);
+  const {active, library} = useWeb3React();
+  const {loading, punks} = usePlatziPunksData({
+    owner: (submitted && validAddress) ? address : null
+  });
+  const { push } = useHistory()
   
 
   if (!active) return <RequestAccess />
 
+  const submit = (event) => {
+    event.preventDefault()
+
+    console.log('address:', address)
+    if (address) {
+      const isValidAddress = library.utils.isAddress(address);
+      setSubmitted(true);
+      setValidAddress(isValidAddress);
+      if (isValidAddress) push(`/punks?address=${address}`)
+    } else {
+      push("/punks")
+    }
+  }
+
+  const handleInputAddress = ({target: {value}}) => {
+    setSubmitted(false)
+    setValidAddress(false)
+    setAddress(value)
+  }
+
   return(
     <>
-      <form onSubmit={() => {console.log('submit !!')}}>
+      <form onSubmit={submit}>
         <FormControl>
           <InputGroup mb={3}>
             <InputLeftElement
               pointerEvents="none"
-              children={<SearchIcon color="gray.300" />}
+              children={<SearchIcon color="gray.600" />}
             />
             <Input
               isInvalid={false}
               value={address ?? ""}
-              onChange={() => {}}
+              onChange={handleInputAddress}
               placeholder="Buscar por dirección"
             />
             <InputRightElement width="5.5rem">
@@ -47,24 +76,27 @@ const Punks = () => {
               </Button>
             </InputRightElement>
           </InputGroup>
-          {/* {submitted && !validAddress && (
+          {submitted && !validAddress && (
             <FormHelperText>Dirección inválida</FormHelperText>
-          )} */}
+          )}
         </FormControl>
       </form>
-      {loading ? <Loading /> :
-      <>
-        <Text fontSize='5xl' color='gray.500' align="center" mb={4}>Gallery</Text>
-        <Grid templateColumns="repeat(auto-fill, minmax(250px, 1fr))" gap={6}>
-            {punks.map(({tokenId, name, image}) => {
-              return (
-                <Link to={`/punk/${tokenId}`} key={tokenId}>
-                  <PunkCard name={name} image={image} />
-                </Link>
-              )
-            })}  
-        </Grid>
-      </>
+      {loading ? (
+        <Loading />
+      )  : (
+        <>
+          <Text fontSize='5xl' color='gray.500' align="center" mb={4}>Gallery</Text>
+          <Grid templateColumns="repeat(auto-fill, minmax(250px, 1fr))" gap={6}>
+              {punks.map(({tokenId, name, image}) => {
+                return (
+                  <Link to={`/punk/${tokenId}`} key={tokenId}>
+                    <PunkCard name={name} image={image} />
+                  </Link>
+                )
+              })}  
+          </Grid>
+        </>
+      )
       }
     </>
   )
