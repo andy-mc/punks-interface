7ae08e119cc6399670fa31775c34edba2beda9dd
Andrés Muñoz <andres.munoz@idukay.com>
2022-01-16 21:30:21 -0500

    add input filter sear to gallery

--- src/views/punks/index.js
+++ src/views/punks/index.js
@@ -1,12 +1,25 @@
 import { useWeb3React } from '@web3-react/core';
-import { Grid, Text } from '@chakra-ui/react';
+import {
+  Grid,
+  Text,
+  InputGroup,
+  InputLeftElement,
+  Input,
+  InputRightElement,
+  Button,
+  FormHelperText,
+  FormControl,
+} from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
 import Loading from '../../components/loading';
 import PunkCard from '../../components/punk-card';
 import RequestAccess from '../../components/request-access';
 import {usePlatziPunksData} from '../../hooks/usePlatziPunksData';
 import { Link } from "react-router-dom";
+import { useState } from 'react';
 
 const Punks = () => {
+  const [address, setAddress] = useState('')
   const {active} = useWeb3React();
   const {loading, punks} = usePlatziPunksData();
   
@@ -15,6 +28,30 @@ const Punks = () => {
 
   return(
     <>
+      <form onSubmit={() => {console.log('submit !!')}}>
+        <FormControl>
+          <InputGroup mb={3}>
+            <InputLeftElement
+              pointerEvents="none"
+              children={<SearchIcon color="gray.300" />}
+            />
+            <Input
+              isInvalid={false}
+              value={address ?? ""}
+              onChange={() => {}}
+              placeholder="Buscar por dirección"
+            />
+            <InputRightElement width="5.5rem">
+              <Button type="submit" h="1.75rem" size="sm">
+                Buscar
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+          {/* {submitted && !validAddress && (
+            <FormHelperText>Dirección inválida</FormHelperText>
+          )} */}
+        </FormControl>
+      </form>
       {loading ? <Loading /> :
       <>
         <Text fontSize='5xl' color='gray.500' align="center" mb={4}>Gallery</Text>
